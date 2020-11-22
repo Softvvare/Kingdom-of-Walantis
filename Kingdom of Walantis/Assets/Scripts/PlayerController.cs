40509632116c3acd5f7472af4186248516d64486
Ramazan Emre Erkan <remre98@gmail.com>
2020-11-22 17:59:02 +0300

    Physics and Player Controller adding
    Physics Controller
    Character Controller
    Camera Character Following
    Double jump (?)

---
+++ Kingdom of Walantis/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject
+{
+    public float JumpTakeOffSpeed = 7;
+    public float MaxSpeed = 7;
+    bool canDoubleJump = false;
+
+    void Start()
+    {
+        
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 MoveP = Vector2.zero;
+        MoveP.x = Input.GetAxis("Horizontal");
+
+      
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            if (grounded)
+            {
+                canDoubleJump = true;
+                velocity.y = JumpTakeOffSpeed;
+                Debug.Log(velocity.y);
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    canDoubleJump = false;
+                    velocity.y += JumpTakeOffSpeed;
+                    Debug.Log(velocity.y);
+                }
+            }
+            
+        }
+        
+        /*
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.50f;
+            }
+        }
+    */
+        targetVelocity = MoveP * MaxSpeed;
+    }
+
+
+}
