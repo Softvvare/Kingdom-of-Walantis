04b266b86dee5d0e9297d037a75a44294d60cd82
Oguz Tozkoparan <oguztozkoparan@users.noreply.github.com>
2020-12-23 15:39:05 +0300

    Fight System
    + Attack
    + Temp enemy which is King
    + Die and Hurt animations on enemy
    + Attack Cooldown
    + Attack radius with Empty object which has transform positions

---
+++ Kingdom of Walantis/Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : PhysicsObject
+{
+
+    private Animator anim;
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Damage Taken");
+        // play hurt animaton ekle, bool isDead
+        anim.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died");
+
+        anim.SetBool("isDead", true);
+        // die animation  // Enemy Animation Controller
+
+        //disable enemy
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+
+}
