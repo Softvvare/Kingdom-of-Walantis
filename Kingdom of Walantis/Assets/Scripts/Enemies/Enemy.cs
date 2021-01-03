0c925b2a465b9e12cc9c72aca744604f7c807d1c
Ramazan Emre Erkan <remre98@gmail.com>
2021-01-03 21:16:52 +0300

    Move Towards function
    Move Towards function added

--- Kingdom of Walantis/Assets/Scripts/Enemies/Enemy.cs
+++ Kingdom of Walantis/Assets/Scripts/Enemies/Enemy.cs
@@ -6,15 +6,19 @@ public class Enemy : PhysicsObject
 {
 
     private Animator anim;
-    
-    public int 
+
+    public int
         maxHealth = 100,
         currentHealth;
 
-    public float 
-        MaxSpeed = 7;
+    [SerializeField]
+    private float
+        MaxSpeed;
 
-    private Transform target;
+    [SerializeField]
+    private Transform 
+        PlayerChecker,
+        PlayerObj;
 
     private bool
         flip,
@@ -24,10 +28,19 @@ public class Enemy : PhysicsObject
     [SerializeField]
     private Transform rotateP;
 
-
-    public LayerMask groundLayer;
+    [SerializeField]
+    private LayerMask 
+        groundLayer,
+        playerLayer;
 
     public Vector2 MoveP = Vector2.zero;
+
+    // new part 
+
+    [SerializeField]
+    private float agroRange;
+    
+    // end of new part
 
 
     void Start()
@@ -39,8 +52,48 @@ public class Enemy : PhysicsObject
         flip = true;
         canRun = true;
         MoveP.x = 1f;
+        agroRange = 4;
+        MaxSpeed = 3;
+
     }
 
+
+    protected override void MoveTowards()
+    {
+        //transform.position = Vector2.MoveTowards(transform.position, PlayerObj.transform.position, MaxSpeed * Time.deltaTime);
+
+        /*        float distToPlayer = Vector2.Distance(transform.position, PlayerObj.position);
+
+                if (distToPlayer < agroRange)
+                {
+                    if (transform.position.x < PlayerObj.position.x)
+                    {
+                        MoveP.x = 1f;
+                    }
+                    else
+                    {
+                        MoveP.x = -1f;
+                    }
+                }*/
+
+
+        if (CheckFlip())
+        {
+            if (MoveP.x > 0 && LookRight == true)
+            {
+                Flip();
+
+                MoveP.x = -1f;
+            }
+            else if (MoveP.x < 0 && LookRight == false)
+            {
+                Flip();
+
+                MoveP.x = 1f;
+            }
+        }
+    }
+ 
 
     public void TakeDamage(int damage)
     {
@@ -67,7 +120,6 @@ public class Enemy : PhysicsObject
         this.enabled = false;
     }
 
-
     protected override void ComputeVelocity()
     {
 
@@ -84,20 +136,15 @@ public class Enemy : PhysicsObject
                 Flip();
 
                 MoveP.x = -1f;
-                Debug.Log("If" + MoveP);
-
             }
             else if (MoveP.x < 0 && LookRight == false)
             {
                 Flip();
 
                 MoveP.x = 1f;
-                Debug.Log("Else" + MoveP);
             }
 
         }
-
-
 
         // set animation parameters
         // anim.SetBool("Grounded", grounded);
@@ -106,19 +153,18 @@ public class Enemy : PhysicsObject
 
 
         targetVelocity = MoveP * MaxSpeed;
-
     }
+
     private void Flip()
     {
 
         if (CheckFlip() == true)
         {
-            LookRight = !LookRight;
-
             Vector3 Scale = transform.localScale;
             Scale.x *= -1;
             transform.localScale = Scale;
             flip = false;
+            LookRight = !LookRight;
         }
 
     }
@@ -144,27 +190,102 @@ public class Enemy : PhysicsObject
     }
 
 
-    public bool CheckFlip()
+    public bool DistanceChecker()
     {
-        Collider2D detectedObject = Physics2D.OverlapCircle(rotateP.position, 0.1f, groundLayer);
+        float distToPlayer = Vector2.Distance(transform.position.normalized, PlayerObj.position);
 
-        if (detectedObject == null)
+        if (distToPlayer < agroRange)
         {
-            flip = false;
-            return false;
+            Debug.Log("True" + distToPlayer);
+            return true;
         }
         else
         {
-            flip = true;
-            return true;
+            Debug.Log("False" + distToPlayer);
+            return false;
+        }
+    }
+
+    public bool CheckFlip()
+    {
+        Collider2D detectedObject = Physics2D.OverlapCircle(rotateP.position, 0.1f, groundLayer);
+        Collider2D detectedPlayer = Physics2D.OverlapCircle(PlayerChecker.position, agroRange, playerLayer);
+   
+
+        if (detectedObject == null && detectedPlayer == null)
+        {
+            if (!DistanceChecker())
+            {
+                flip = false;
+                return false;
+            }
+            else
+            {
+                flip = true;
+                return true;
+            }
+            
+        }
+
+        else if (detectedObject != null && detectedPlayer == null)
+        {
+            if (!DistanceChecker())
+            {
+                flip = true;
+                return true;
+            }
+            else
+            {
+                flip = true;
+                return true;
+            }
+
         }
         
-        //detectedObject.GetComponent<Enemy>().Flip();
-        
+        else if (detectedObject == null && detectedPlayer != null)
+        {
+            if (DistanceChecker())
+            {
+                flip = false;
+                return false;
+            }
+            else
+            {
+                flip = false;
+                return false; 
+            }
+
+        }
+       
+        else // if(detectedObject != null && detectedPlayer != null)
+        {
+            if (DistanceChecker())
+            {
+                flip = false;
+                return false;
+            }
+            else
+            {
+                flip = false;
+                return false;
+            }
+
+        }
+
     }
+
 
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(PlayerChecker.position, agroRange);
+
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(rotateP.position, 0.1f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, PlayerObj.position);
+        
     }
 }
+
--- Kingdom of Walantis/Assets/Scripts/PhysicsObject.cs
+++ Kingdom of Walantis/Assets/Scripts/PhysicsObject.cs
@@ -14,8 +14,8 @@ public class PhysicsObject : MonoBehaviour
     protected Rigidbody2D rb2d;
     protected Vector2 velocity;
     protected ContactFilter2D contactFilter;
-    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
-    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[2];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(2);
 
     protected const float minMoveDistance = 0.001f;
     protected const float shellRadius = 0.01f;
@@ -38,12 +38,19 @@ public class PhysicsObject : MonoBehaviour
     {
         targetVelocity = Vector2.zero;
         ComputeVelocity();
+        MoveTowards();
     }
 
     protected virtual void ComputeVelocity()
     {
 
     }
+
+    protected virtual void MoveTowards()
+    {
+
+    }
+
 
     private void FixedUpdate()
     {
